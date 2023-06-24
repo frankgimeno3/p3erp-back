203d8d8bd7e37ebf83992a4a00855a638c925c91
Frank Gimeno <frankgimeno3@gmail.com>
2023-06-24 14:55:56 +0200

    añado model notificaciones

---
+++ models/notificaciones.model.js
@@ -0,0 +1,14 @@
+import mongoose from 'mongoose';
+
+const notificacionSchema = new mongoose.Schema({
+  cuentareceptora: { type: String, required: true, unique: true },
+  cuentaemisora: { type: String, required: true },
+// visible es booleana inicializada en true
+// timestamps creacion
+// timestamps desde que se ocultó (sirve para un historial), se borran cuando llegan a x tiempo
+// contenido en textp
+});
+
+const User = mongoose.model('Notificacion', notificacionSchema);
+
+export default Notificacion;
