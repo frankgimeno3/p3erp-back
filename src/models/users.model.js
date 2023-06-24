9bf89978a324cba444578164fef506518af2ce0b
Frank Gimeno <frankgimeno3@gmail.com>
2023-06-24 15:12:17 +0200

    termino de conectar, ya funciona como back

--- src/models/users.model.js
+++ src/models/users.model.js
@@ -5,6 +5,7 @@ const userSchema = new mongoose.Schema({
   email: { type: String, required: true, unique: true },
   user: { type: String, required: true, unique: true },
   password: { type: String, required: true },
+  userrole: { type: String, required: true },
 });
 
 const User = mongoose.model('User', userSchema);
