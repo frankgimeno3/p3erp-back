de7dfdd2f44e25e0bedb2bf8a74719d94084c713
Frank Gimeno <frankgimeno3@gmail.com>
2023-06-25 23:59:10 +0200

    añado timestamps, opcion itsvisible, timestamps desde visible

--- src/models/notificaciones.model.js
+++ src/models/notificaciones.model.js
@@ -4,12 +4,14 @@ const notificacionSchema = new mongoose.Schema({
   cuentareceptora: { type: String, required: true, unique: true },
   cuentaemisora: { type: String, required: true },
   contenido: { type: String, required: true },
+  timestampscreation: { type: String, required: true },
+  timestampshidden: { type: String, required: true },
+  itsvisible: { type: String, required: true },
 
 // visible es booleana inicializada en true
 // timestamps creacion
 // timestamps desde que se ocultó (sirve para un historial), se borran cuando llegan a x tiempo
-// contenido en textp
-});
+ });
 
 const Notificacion = mongoose.model('Notificacion', notificacionSchema);
 
