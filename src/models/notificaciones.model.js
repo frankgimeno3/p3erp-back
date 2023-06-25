5eeb9138eb6d4ca7a3a4a097d40a856645bb8584
Frank Gimeno <frankgimeno3@gmail.com>
2023-06-25 23:57:35 +0200

    añado contenido a notificaciones

--- src/models/notificaciones.model.js
+++ src/models/notificaciones.model.js
@@ -3,6 +3,8 @@ import mongoose from 'mongoose';
 const notificacionSchema = new mongoose.Schema({
   cuentareceptora: { type: String, required: true, unique: true },
   cuentaemisora: { type: String, required: true },
+  contenido: { type: String, required: true },
+
 // visible es booleana inicializada en true
 // timestamps creacion
 // timestamps desde que se ocultó (sirve para un historial), se borran cuando llegan a x tiempo
